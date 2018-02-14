d834ba6edc1d6cc8b686ca3de5af8815c13b229f
Aditya Pakki <pakki001@umn.edu>
2018-02-13 23:24:42 -0600

    Update the Test cases for the google pub sub

--- SPubSub/simplepubsub/gcpsClient.go
+++ SPubSub/simplepubsub/gcpsClient.go
@@ -10,38 +10,41 @@ import (
   "google.golang.org/api/iterator"
 )
 
+type Client struct {
+    cli *pubsub.Client
+    ctx context.Context
+}
 
-func Join() (c *pubsub.Client, ctx context.Context) {
-  ctx = context.Background()
+func  Join() (client Client) {
+  ctx := context.Background()
   // Sets your Google Cloud Platform project ID.
   projectID := "pubsub-194903"
   // Creates a client.
-  c, err := pubsub.NewClient(ctx, projectID)
+  cli, err := pubsub.NewClient(ctx, projectID)
   if err != nil {
-    log.Fatalf("Failed to create client: %v", err)
+    log.Fatalf("Unable to create a client")
   }
-  return c, ctx
+  return  Client{cli, ctx}
 }
 
-func CreateTopic( c *pubsub.Client,ctx context.Context, 
-         topicName string) (topic *pubsub.Topic, err error) {
+func (client *Client) CreateTopic(topicName string) (topic *pubsub.Topic) {
   // Creates the new topic.
-  topic, err = c.CreateTopic(ctx, topicName)
+  topic, err := client.cli.CreateTopic(client.ctx, topicName)
   if err != nil {
     log.Fatalf("Failed to create topic: %v", err)
   }
   fmt.Println("Successful in creating topic :%v", topic)
-  return topic,err
+  return topic
 }
 
 //Subscribe to one or more topics at the same time
-func Subscribe( c *pubsub.Client,ctx context.Context,
-              topic *pubsub.Topic) (sub *pubsub.Subscription,err error) {
-  ok, err := topic.Exists(ctx)
+func (client *Client) Subscribe(topic *pubsub.Topic) (sub *pubsub.Subscription,
+                                                      err error) {
+  ok, err := topic.Exists(client.ctx)
   if err == nil {
     val := topic.ID()
     fmt.Println("subscribing to ", val)
-    sub,err =  c.CreateSubscription(ctx, val, pubsub.SubscriptionConfig {
+    sub,err =  client.cli.CreateSubscription(client.ctx, val, pubsub.SubscriptionConfig {
                 Topic:topic,AckDeadline: 10 * time.Second,
                 })
   }
@@ -52,18 +55,17 @@ func Subscribe( c *pubsub.Client,ctx context.Context,
 }
 
 //Unsubscribe from one or more topics at the same time 
-func Unsubscribe( c *pubsub.Client,ctx context.Context, 
-                  topic *pubsub.Topic) (err error){
-  _, err = topic.Exists(ctx)
+func (client *Client) Unsubscribe(topic *pubsub.Topic) (err error){
+  _, err = topic.Exists(client.ctx)
   if err != nil {
     log.Fatalf("Topic doesn't exist to unsubscribe")
   }
-  for subs := topic.Subscriptions(ctx) ;; {
+  for subs := topic.Subscriptions(client.ctx) ;; {
     sub, err := subs.Next()
     if err == iterator.Done {
       break
     }
-    if err = sub.Delete(ctx); err == nil {
+    if err = sub.Delete(client.ctx); err == nil {
       fmt.Println("unsubscribing from ",topic.ID())
     }
   }
@@ -71,20 +73,20 @@ func Unsubscribe( c *pubsub.Client,ctx context.Context,
 }
 
 //PUblish message msg to the Subscribers
-func Publish(c *pubsub.Client, ctx context.Context, topic *pubsub.Topic, 
-                        msg string) (err error) {
-  result := topic.Publish(ctx, &pubsub.Message{
+func (client *Client) Publish(topic *pubsub.Topic, msg string) (err error) {
+  result := topic.Publish(client.ctx, &pubsub.Message{
             Data: []byte(msg),
           })
-  id, err := result.Get(ctx)
+  id, err := result.Get(client.ctx)
   fmt.Printf("Published a message; msg ID: %v\n", id)
   return err
 }
 
-func Consume(c *pubsub.Client, ctx context.Context, 
-              sub *pubsub.Subscription) (err error) {
+
+func (client *Client) Consume(sub *pubsub.Subscription) (err error) {
   var mu sync.Mutex
   received := 0
+  ctx := client.ctx
   cctx, cancel := context.WithCancel(ctx)
   err = sub.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
     mu.Lock()
@@ -98,5 +100,11 @@ func Consume(c *pubsub.Client, ctx context.Context,
     fmt.Printf("Got message: %q\n", string(msg.Data))
     msg.Ack()
   })
+  return err
+}
+
+//close the client to retrieve all resouces
+func (client *Client) Cleanup() (err error) {
+  err = client.cli.Close()
   return err
 }
--- SPubSub/test.go
+++ SPubSub/test.go
@@ -4,53 +4,72 @@ import (
   "log"
   "os"
   "simplepubsub"
-// Imports the Google Cloud Pub/Sub client package.
   "cloud.google.com/go/pubsub"
-  "golang.org/x/net/context"
+  "math/rand"
 )
 
-type ClientTest struct {
-    c *pubsub.Client
-    ctx context.Context
-}
-
 func main() {
-
-  c,ctx := simplepubsub.Join()
+  c := simplepubsub.Join()
   // Sets the name for the new topic.
   topicName := "Jenka2"
   if len(os.Args) == 2 {
     topicName = os.Args[1]
   }
   // Creates the new topic.
-  topic, err := simplepubsub.CreateTopic(c, ctx, topicName)
-  if err != nil {
-    fmt.Println("Failed to create topic: %v", err)
-  }
-
+  topic := c.CreateTopic(topicName)
   // create a list of subscriptions
   // Returning a subscription is not important but usefule during testing
   // instead of taking a convoluted path of extracting the Topic and generating
   // the subscriptions
-  sub, err := simplepubsub.Subscribe(c,ctx, topic)
+  sub, err := c.Subscribe(topic)
   if err != nil {
     fmt.Println("Failed to subscribe as topic doesn't exist")
   }
 
-  _ = simplepubsub.Publish(c,ctx, topic, "lafoot")
-  err = simplepubsub.Publish(c,ctx, topic, "lafoot2")
+  _ = c.Publish(topic, "simple message")
+  err = c.Publish(topic, "complex message")
   if err != nil {
-    fmt.Println(err)
+    fmt.Println("Failed to publish to the server")
   }
   // Consume the message that was sent by the publish messages
-  err = simplepubsub.Consume(c, ctx, sub)
+  err = c.Consume(sub)
   if err != nil {
-    fmt.Println(err)
+    fmt.Println("Failed to consume the messages at the subscribers")
   }
+
   // delete the subscritption based on the topic
-  err = simplepubsub.Unsubscribe(c,ctx, topic)
+  err = c.Unsubscribe(topic)
   if err != nil {
     log.Fatalf("Failed to Unsubscribe as topic doesn't exist")
   }
+
+  if c.Cleanup() != nil {
+    log.Fatalf("Failed to close all the resources of a client")
+  }
+
+  // Run a set of clients instead of a single client
+  var clist [3]simplepubsub.Client
+  for i:=0; i < len(clist); i++ {
+    clist[i] = simplepubsub.Join()
+  }
+
+  var t []*pubsub.Topic
+  t = append(t,clist[1].CreateTopic("Roxette"))
+  t = append(t,clist[1].CreateTopic("ManUtd"))
+  t = append(t,clist[1].CreateTopic("TaylorSwift"))
+  t = append(t,clist[0].CreateTopic("DaysofThunder"))
+  t = append(t,clist[0].CreateTopic("ChelseaFC"))
+  t = append(t,clist[0].CreateTopic("SandraBullock"))
+  t = append(t,clist[2].CreateTopic("KatyPerry"))
+  t = append(t,clist[2].CreateTopic("Somethingsomething"))
+  t = append(t,clist[2].CreateTopic("MinnesotaUnited"))
+
+  //Randomly subscribe to a different topic for a different client
+  for i:=0; i < 6; i++ {
+    client := rand.Int() % len(clist)
+    top_rand := rand.Int()% len(t)
+    clist[client].Subscribe(t[top_rand])
+  }
+
 
 }
